0479a259e64ed2a3a5ae824888ca7774e70f6c4d
Petiton Wiseley Paul-Enzer <wppet@ulaval.ca>
2026-07-18 06:23:35 -0400

    refactor: update to allow enum string in json request body

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,7 +6,10 @@ using VehicleMaintenanceTracker.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions.Converters.Add(
+            new System.Text.Json.Serialization.JsonStringEnumConverter()));
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
