3852def57a219dd05fb6892e04b80570acb661ee
Petiton Wiseley Paul-Enzer <wppet@ulaval.ca>
2026-07-18 03:34:16 -0400

    refactor: update the entry point to add the global exception

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using VehicleMaintenanceTracker.Data;
+using VehicleMaintenanceTracker.Exceptions;
 using VehicleMaintenanceTracker.Services;
+using VehicleMaintenanceTracker.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +15,15 @@ builder.Services.AddScoped<IVehicleService, VehicleService>();
 builder.Services.AddScoped<IMaintenanceService, MaintenanceService>();
 builder.Services.AddScoped<IDashboardService, DashboardService>();
 
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseExceptionHandler();
 
 if (app.Environment.IsDevelopment())
 {
@@ -25,8 +32,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseRouting();
 
 app.MapControllers();
 
